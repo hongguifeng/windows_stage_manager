146c513391b67eef1829f55863ed835ef0f35d18
hongguifeng <hongguifeng@126.com>
2026-09-05 12:47:59 +0800

    release: prepare center-oriented rc11

--- src/app/version.h
+++ src/app/version.h
@@ -7,7 +7,7 @@ namespace stage_manager::app {
 inline constexpr int kVersionMajor = 0;
 inline constexpr int kVersionMinor = 1;
 inline constexpr int kVersionPatch = 0;
-inline constexpr std::string_view kVersion = "0.1.0-rc10";
+inline constexpr std::string_view kVersion = "0.1.0-rc11";
 inline constexpr std::string_view kProductName = "WindowsStageManager";
 
 } // namespace stage_manager::app
